56b8fdcb638064638b9a4cd0de8627f13edf72f1
aguiar231 <aguiarbruno231@gmail.com>
2024-10-01 21:51:57 -0300

    tentei criar as paginas, porem as rotas nao estao respondendo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from starlette.requests import Request
+from util import ler_html, salvar_cadastro, salvar_contato
 
 app = FastAPI()
 
@@ -13,6 +14,25 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/", response_class=HTMLResponse)
 def paginaInicial(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/cadastro")
+def get_cadastro(request: Request):
+    html = ler_html("cadastro")
+    return HTMLResponse(html)
+
+@app.post("/post_cadastro")
+def post_cadastro(
+    request: Request, 
+    nome: str = Form(...), 
+    data_nascimento: str = Form(...), 
+    email: str = Form(...), 
+    senha: str = Form(...), 
+    confirmacao_senha: str = Form(...)):
+    if senha == confirmacao_senha:
+        salvar_cadastro(nome, data_nascimento, email, senha)
+        return RedirectResponse("/cadastro_recebido", 303)
+    else:
+        return RedirectResponse("/cadastro", 303)
 
 if __name__== "_main_":
     import uvicorn
---
+++ util.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+NOME_PASTA_HTML="html/"
+NOME_PASTA_CADASTROS="dados/cadastros/"
+
+
+def ler_html(nome_arquivo: str) -> str:
+    caminho_arquivo_html = f"{NOME_PASTA_HTML}{nome_arquivo}.html"
+    with open(caminho_arquivo_html, "r", encoding="utf-8") as arquivo:
+        conteudo_html = arquivo.read()
+    return conteudo_html
+
+
+def salvar_cadastro(nome, data_nascimento, email, senha):
+    agora = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    nome_arquivo = f"{NOME_PASTA_CADASTROS}cadastro_{agora}.txt"
+    conteudo = f"Nome: {nome}\nData Nascimento: {data_nascimento}\nEmail: {email}\nSenha: {senha}"
+    with open(nome_arquivo, "w") as arquivo:
+        arquivo.write(conteudo)
